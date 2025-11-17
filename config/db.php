088a53b00be0c7b242aab8f2664ba65de29f338a
nurzhan <nurzhanduisenbaev480@mail.ru>
2025-11-17 21:41:24 +0500

    Установка проекта

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=MySQL-5.7;dbname=yii2_basic',
+    'username' => 'admin',
+    'password' => 'admin',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
